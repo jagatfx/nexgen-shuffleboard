d00d57c43f8c55f1fefbf8f93a7e07d62c3d426d
Matt Greenberg <mgreenberg@gmail.com>
2013-03-15 11:45:45 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shuffleboard::Application.config.secret_token = '85403ba0044baf9283d26d63c056e062fd10ab8adfefcf87db258464647f0e2942483bf574258bbc47ae9ca490af542a10568cabf0f344754e77d927028fb406'
