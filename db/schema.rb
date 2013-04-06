2f13e6196768ed9002f0f0612a786309874071c3
Matt Greenberg <mgreenberg@gmail.com>
2013-04-06 10:52:46 -0700

    Added all the new form changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130323031832) do
+ActiveRecord::Schema.define(:version => 20130406041336) do
 
   create_table "players", :force => true do |t|
     t.string   "name"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(:version => 20130323031832) do
     t.integer  "wins"
     t.integer  "losses"
     t.integer  "rating"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+    t.integer  "singles_rating"
+    t.integer  "doubles_rating"
   end
 
   create_table "results", :force => true do |t|
@@ -28,11 +30,16 @@ ActiveRecord::Schema.define(:version => 20130323031832) do
     t.integer  "away_score"
     t.integer  "home_rating"
     t.integer  "away_rating"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.integer  "home_player_id"
     t.integer  "away_player_id"
     t.integer  "rating_change"
+    t.string   "type"
+    t.integer  "home_partner_rating"
+    t.integer  "away_partner_rating"
+    t.integer  "home_partner_id"
+    t.integer  "away_partner_id"
   end
 
 end
